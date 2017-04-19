32720f4712fc965f017e16a858b32b1f7c316e15
Amos Chen <amoschen614@gmail.com>
2017-04-19 13:41:54 -0700

    Parameterize HTTP port

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const server = http.createServer(function(request, response) {
+	const urlPath = url.parse(request.url).pathname;
+	let filePath = `./client/${urlPath}`;
+	fs.stat(filePath, function(err, fileInfo) {
+		if (err) {
+			response.statusCode = 404;
+			response.end(`Resource not found: "${urlPath}"`);
+		} else {
+			if (fileInfo.isDirectory()) {
+				filePath += '/index.html';
+			}
+			fs.readFile(filePath, function(err, data) {
+				if (err) {
+					response.statusCode = 500;
+					response.end(`Server error: "${err}"`);
+				} else {
+					response.end(data.toString('utf-8'));
+				}
+			});
+		}
+	});
+});
+var PORT = process.env.PORT || 5000;
+server.listen(PORT, function() {
+	console.log(`Server listening on port ${PORT}`);
+})
